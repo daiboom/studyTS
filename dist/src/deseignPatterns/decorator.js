32dbcb7b1d03784352c7635110f7986850da6b99
daiboom <dbchoi@aboutfishing.kr>
2023-06-08 21:35:23 +0900

    build

--- dist/src/deseignPatterns/decorator.js
+++ dist/src/deseignPatterns/decorator.js
@@ -1,59 +1,56 @@
 "use strict";
 function Sale(price) {
-  this.price = price;
-  this.decorators_list = [];
+    this.price = price;
+    this.decorators_list = [];
 }
 Sale.prototype.decorate = function (decorator) {
-  this.decorators_list.push(decorator);
+    this.decorators_list.push(decorator);
 };
 Sale.prototype.getPrice = function () {
-  let price = this.price;
-  let i = 0;
-  let max = this.decorators_list.length;
-  let name = null;
-  for (i = 0; i < max; i++) {
-    name = this.decorators_list[i];
-    price = Sale.decorators[name].getPrice(price);
-  }
-  return price;
+    let price = this.price;
+    let i = 0;
+    let max = this.decorators_list.length;
+    let name = null;
+    for (i = 0; i < max; i++) {
+        name = this.decorators_list[i];
+        price = Sale.decorators[name].getPrice(price);
+    }
+    return price;
 };
 Sale.decorators = {};
 Sale.decorators.fedtax = {
-  getPrice: function (price) {
-    return price + (price * 5) / 100;
-  },
+    getPrice: function (price) {
+        return price + (price * 5) / 100;
+    },
 };
 Sale.decorators.quebec = {
-  getPrice: function (price) {
-    return price + (price * 7.5) / 100;
-  },
+    getPrice: function (price) {
+        return price + (price * 7.5) / 100;
+    },
 };
 Sale.decorators.money = {
-  getPrice: function (price) {
-    return price.toFixed(2);
-  },
+    getPrice: function (price) {
+        return price.toFixed(2);
+    },
 };
 Sale.decorators.cdn = {
-  getPrice: function (price) {
-    return "$" + price;
-  },
+    getPrice: function (price) {
+        return "$" + price;
+    },
 };
 function hello() {
-  return {
-    getPrice: function (price) {
-      return "world" + price;
-    },
-  };
+    return {
+        getPrice: function (price) {
+            return "world" + price;
+        },
+    };
 }
 const world = {
-  getPrice: function (price) {
-    return "hello" + price;
-  },
+    getPrice: function (price) {
+        return "hello" + price;
+    },
 };
-Sale.decorators = Object.assign(Object.assign({}, Sale.decorators), {
-  hello: hello(),
-  world,
-});
+Sale.decorators = Object.assign(Object.assign({}, Sale.decorators), { hello: hello(), world });
 console.log(Sale.decorators);
 let sale = new Sale(100);
 sale.decorate("fedtax");
